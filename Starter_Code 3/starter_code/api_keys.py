c4048f0ff0bd856aa6ad3e7dda60b6aa8752c833
Oleslamburger <tannerblattner@icloud.com>
2023-11-10 23:14:57 -0800

    vacation done

--- Starter_Code 3/starter_code/api_keys.py
+++ Starter_Code 3/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "5d0028610652ee0d1f6d4df413a52792"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2cdd3c8561f3409fa98910b096ceb54f"
